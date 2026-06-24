1d3637c5e2f294d91586d1a2e7e8518e1d8a8793
Dani-elle685 <dannjor2@gmail.com>
2026-06-24 09:28:53 +0300

    initial commit

---
+++ app/(dashboardlayout)/layout.tsx
@@ -0,0 +1,18 @@
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={ `h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        
+        {children}
+        </body>
+    </html>
+  );
+}
